1a150eda69b88405073697e8c612b5289a841eef
Smart Plate admin <admin@smartplate.org>
2025-12-04 12:18:56 +0000

    File changes

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader, Save, Upload, X } from "lucide-react";
-import { base44 } from "@/api/base44Client";
 import { useLanguage } from "../components/LanguageProvider";
 
 export default function UserProfilePage() {
