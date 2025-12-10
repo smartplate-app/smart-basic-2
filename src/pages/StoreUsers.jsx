837ec836991b761c29d43df8848ec4dff950a917
Smart Plate admin <admin@smartplate.org>
2025-12-10 17:17:34 +0000

    File changes

--- src/pages/StoreUsers.jsx
+++ src/pages/StoreUsers.jsx
@@ -473,6 +473,7 @@ export default function StoreUsersPage() {
               </div>
             </DialogContent>
           </Dialog>
+          </div>
         </div>
 
         {/* Role Explanation Cards */}
