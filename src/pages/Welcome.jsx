5da79ab5388b13645c96a9697cd1346ad397c70d
Smart Plate admin <admin@smartplate.org>
2025-12-30 12:39:11 +0000

    File changes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -62,6 +62,7 @@ export default function Welcome() {
           </div>
         </div>
       </div>
+      <AccessRequestDialog open={openRequest} onOpenChange={setOpenRequest} />
     </div>
   );
 }
