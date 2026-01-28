737cb7741066e1d2c8eb9c463bdb56029e7c5906
Smart Plate admin <admin@smartplate.org>
2026-01-28 22:41:31 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1004,8 +1004,7 @@ const [monthReceipts, setMonthReceipts] = useState([]);
               <CardContent>
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                   <div className="lg:col-span-1">
-                    {/* Inline widget */}
-                    <div id="credit-widget-inline"></div>
+                    <CreditBalancesWidget />
                   </div>
                   <div className="lg:col-span-2">
                 
