466dc99e4a215ff7b9cb10e6212f4f0c7d1a2341
Smart Plate admin <admin@smartplate.org>
2026-04-05 20:15:09 +0000

    File changes

--- base44/functions/sitemapXml/entry.ts
+++ base44/functions/sitemapXml/entry.ts
@@ -5,22 +5,18 @@ Deno.serve(async (req) => {
     const base = (Deno.env.get('PUBLIC_APP_URL') || origin).replace(/\/$/, '');
     const lastmod = new Date().toISOString();
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>${base}/functions/welcomePublic?lang=he</loc>
-    <lastmod>${lastmod}</lastmod>
-    <xhtml:link rel="alternate" hreflang="he-IL" href="${base}/functions/welcomePublic?lang=he" />
-    <xhtml:link rel="alternate" hreflang="en-US" href="${base}/functions/welcomePublic?lang=en" />
+    <loc>${base}/</loc>
+    <lastmod>${lastmod.split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
   </url>
   <url>
-    <loc>${base}/functions/welcomePublic?lang=en</loc>
-    <lastmod>${lastmod}</lastmod>
-    <xhtml:link rel="alternate" hreflang="he-IL" href="${base}/functions/welcomePublic?lang=he" />
-    <xhtml:link rel="alternate" hreflang="en-US" href="${base}/functions/welcomePublic?lang=en" />
-  </url>
-  <url>
-    <loc>${base}/functions/basic</loc>
-    <lastmod>${lastmod}</lastmod>
+    <loc>${base}/welcome</loc>
+    <lastmod>${lastmod.split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
   </url>
 </urlset>`;
     return new Response(xml, { status: 200, headers: { 'content-type': 'application/xml; charset=utf-8' } });
